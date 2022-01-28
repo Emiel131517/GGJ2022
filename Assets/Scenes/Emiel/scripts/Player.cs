cefe27c498ac6d4e8804404b3c655c5f1e095c48
Emiel131517 <84375895+Emiel131517@users.noreply.github.com>
2022-01-28 23:34:03 +0100

    added player movement

--- Assets/Scenes/Emiel/scripts/Player.cs
+++ Assets/Scenes/Emiel/scripts/Player.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float moveSpeed;
     private Rigidbody2D rb;
-    void Start()
+
+    [SerializeField]
+    private Transform groundCheck;
+    [SerializeField]
+    private LayerMask groundLayer;
+
+    private bool isGrounded;
+    private float moveSpeed;
+    private void Start()
     {
-        moveSpeed = 10f;
+        moveSpeed = 1000;
         rb = GetComponent<Rigidbody2D>();
     }
-    void Update()
+    private void Update()
     {
-        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        rb.MovePosition(rb.position + move * moveSpeed * Time.deltaTime);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        Movement();
+    }
+    private void Movement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(horizontal * moveSpeed * Time.deltaTime, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(0, 5f);
+        }
     }
 }
