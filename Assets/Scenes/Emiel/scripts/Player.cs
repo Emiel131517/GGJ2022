1ce1f8a83ca3f07550589cbbd2fdf88f33fa31c7
Emiel131517 <84375895+Emiel131517@users.noreply.github.com>
2022-01-28 20:19:41 +0100

    scenes

---
+++ Assets/Scenes/Emiel/scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float moveSpeed;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        moveSpeed = 10f;
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rb.MovePosition(rb.position + move * moveSpeed * Time.deltaTime);
+    }
+}
