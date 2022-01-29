29559ccbb459ffe0991aa6b450c7079b3b2b51f6
Emiel131517 <84375895+Emiel131517@users.noreply.github.com>
2022-01-30 00:26:30 +0100

    menus

--- Assets/Scenes/Levels/Scripts/LevelsMenu.cs
+++ Assets/Scenes/Levels/Scripts/LevelsMenu.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelsMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LevelOne()
     {
-        
+        SceneManager.LoadScene("Level_1");
     }
-
-    // Update is called once per frame
-    void Update()
+    public void LevelTwo()
     {
-        
+        Debug.Log("Coming soon!");
+    }
+    public void LevelThree()
+    {
+        Debug.Log("Coming soon!");
+    }
+    public void LevelFour()
+    {
+        Debug.Log("Coming soon!");
+    }
+    public void LevelFive()
+    {
+        Debug.Log("Coming soon!");
+    }
+    public void LevelSix()
+    {
+        Debug.Log("Coming soon!");
     }
 }
