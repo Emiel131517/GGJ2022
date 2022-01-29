01843439962b6e28a9708669b9bfae60071f50a4
Emiel131517 <84375895+Emiel131517@users.noreply.github.com>
2022-01-29 14:16:05 +0100

    Fixed Portals

---
+++ Assets/Scenes/Emiel/scripts/Moth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float moveSpeed;
+
+    private float moveArea;
+    private float moveAreaMax;
+    private float moveAreMin;
+    void Start()
+    {
+        moveSpeed = 500;
+
+        moveAreaMax = 100;
+        moveAreMin = -100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveArea < 100)
+        {
+
+        }
+    }
+    private void MoveRight()
+    {
+        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+    }
+}
--- Assets/Scenes/Emiel/scripts/Portal.cs
+++ Assets/Scenes/Emiel/scripts/Portal.cs
@@ -9,62 +9,40 @@ public class Portal : MonoBehaviour
     public GameObject normalWorldScene;
     public GameObject ghostWorldScene;
     public Transform endPoint;
-    private bool normalWorld;
     void Start()
     {
-        if (normalWorld)
-        {
-            normalWorldScene.SetActive(true);
-            ghostWorldScene.SetActive(false);
-        }
-        normalWorld = true;
+        normalWorldScene.SetActive(true);
+        ghostWorldScene.SetActive(false);
         player = GameObject.Find("Player").GetComponent<Player>();
     }
-
-    void Update()
-    {
-        if (normalWorld == true)
-        {
-            normalWorldScene.SetActive(true);
-            ghostWorldScene.SetActive(false);
-        }
-        if (normalWorld == false)
-        {
-            normalWorldScene.SetActive(false);
-            ghostWorldScene.SetActive(true);
-        }
-        Debug.Log(normalWorld);
-    }
-/*    public void ChangeWorld(int w)
+    public void ChangeWorld(int w)
     {
         if (w.Equals(0))
         {
-            normalWorld = true;
             player.ChangeSprite(player.normalSprite);
             normalWorldScene.SetActive(true);
             ghostWorldScene.SetActive(false);
+            player.transform.position = endPoint.transform.position;
         }
         if (w.Equals(1))
         {
-            normalWorld = false;
             player.ChangeSprite(player.ghostSprite);
             normalWorldScene.SetActive(false);
             ghostWorldScene.SetActive(true);
+            player.transform.position = endPoint.transform.position;
         }
-    }*/
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            if (normalWorld)
+            if (normalWorldScene.activeSelf)
             {
-                normalWorld = false;
-                player.transform.position = endPoint.transform.position;
+                ChangeWorld(1);
             }
-            if (!normalWorld)
+            else if (!normalWorldScene.activeSelf)
             {
-                normalWorld = true;
-                player.transform.position = endPoint.transform.position;
+                ChangeWorld(0);
             }
         }
     }
