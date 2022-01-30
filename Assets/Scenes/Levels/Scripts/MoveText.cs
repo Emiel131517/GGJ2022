e9c104586edd54e33ccbfea6e780041228118d0d
Emiel131517 <84375895+Emiel131517@users.noreply.github.com>
2022-01-30 16:39:31 +0100

    Final

---
+++ Assets/Scenes/Levels/Scripts/MoveText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveText : MonoBehaviour
+{
+    public float amplitude = 1;
+    public float speed = 1;
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = startPosition + new Vector3(0.0f, Mathf.Sin(Time.time * speed) * amplitude, 0.0f);
+    }
+}
