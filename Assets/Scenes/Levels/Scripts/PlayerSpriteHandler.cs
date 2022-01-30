ac98ddee0eeab4003cdf019bcf48a6653e3a4467
Emiel131517 <84375895+Emiel131517@users.noreply.github.com>
2022-01-30 14:56:56 +0100

    player changes

---
+++ Assets/Scenes/Levels/Scripts/PlayerSpriteHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpriteHandler : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private SpriteRenderer backgroundSpriteRenderer;
+    public Sprite normalBackground;
+    public Sprite ghostBackground;
+    public Sprite ghostSprite;
+    public Sprite normalSprite;
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        backgroundSpriteRenderer = GameObject.Find("Background").GetComponent<SpriteRenderer>();
+
+        spriteRenderer.sprite = normalSprite;
+        backgroundSpriteRenderer.sprite = normalBackground;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangeSprite(Sprite newPlayerSprite, Sprite newBackgroundSprite)
+    {
+        spriteRenderer.sprite = newPlayerSprite;
+        backgroundSpriteRenderer.sprite = newBackgroundSprite;
+    }
+}
